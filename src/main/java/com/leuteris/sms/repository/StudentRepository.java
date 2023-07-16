b3e0291b91e4fa3528007414e6a1a0b07d6bd4cf
egiannar <110569627+egiannar@users.noreply.github.com>
2023-07-16 18:29:39 +0300

    Add files via upload

---
+++ src/main/java/com/leuteris/sms/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.leuteris.sms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.leuteris.sms.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
